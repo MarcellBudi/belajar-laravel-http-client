be433bab5b4261cee04cbd5fad34348b7ff2aca9
MarcellBudi <marcellbudiputra@gmail.com>
2024-02-08 12:41:10 +0800

    upload belajar laravel http client

---
+++ tests/Feature/HttpTest.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\Client\RequestException;
+use Illuminate\Support\Facades\Http;
+use Tests\TestCase;
+
+class HttpTest extends TestCase
+{
+    public function testGet()
+    {
+        $response = Http::get("https://enkizmujozksi.x.pipedream.net");
+        self::assertTrue($response->ok());
+    }
+
+    public function testPost()
+    {
+        $response = Http::post("https://enkizmujozksi.x.pipedream.net");
+        self::assertTrue($response->ok());
+    }
+
+    public function testDelete()
+    {
+        $response = Http::delete("https://enkizmujozksi.x.pipedream.net");
+        self::assertTrue($response->ok());
+    }
+
+    public function testResponse()
+    {
+        $response = Http::get("https://enkizmujozksi.x.pipedream.net");
+        self::assertEquals(200, $response->status());
+        self::assertNotNull($response->headers());
+        self::assertNotNull($response->body());
+
+        $json = $response->json();
+        self::assertTrue($json['success']);
+    }
+
+    public function testQueryParameter()
+    {
+        $response = Http::withQueryParameters([
+            'page' => 1,
+            'limit' => 10,
+        ])->get("https://enkizmujozksi.x.pipedream.net");
+        self::assertTrue($response->ok());
+    }
+
+    public function testHeader()
+    {
+        $response = Http::withQueryParameters([
+            'page' => 1,
+            'limit' => 10,
+        ])->withHeaders([
+            'Accept' => 'application/json',
+            'X-Request-ID' => '123123123',
+        ])->get("https://enkizmujozksi.x.pipedream.net");
+        self::assertTrue($response->ok());
+    }
+
+    public function testCookie()
+    {
+        $response = Http::withQueryParameters([
+            'page' => 1,
+            'limit' => 10,
+        ])->withHeaders([
+            'Accept' => 'application/json',
+            'X-Request-ID' => '123123123',
+        ])->withCookies([
+            "SessionId" => "3242432423",
+            "UserId" => "marcell",
+        ], "enkizmujozksi.x.pipedream.net")->get("https://enkizmujozksi.x.pipedream.net");
+        self::assertTrue($response->ok());
+    }
+
+    public function testFormPost()
+    {
+        $response = Http::asForm()->post("https://enkizmujozksi.x.pipedream.net", [
+            "username" => "admin",
+            "password" => "admin"
+        ]);
+        self::assertTrue($response->ok());
+    }
+
+    public function testMultipartFile()
+    {
+        $response = Http::asMultipart()
+            ->attach("profile", file_get_contents(__DIR__ . '/HttpTest.php') , "profile.jpg")
+            ->post("https://enkizmujozksi.x.pipedream.net", [
+                "username" => "admin",
+                "password" => "admin"
+            ]);
+        self::assertTrue($response->ok());
+    }
+    // public function testMultipartImage()
+    // {
+    //     $response = Http::asMultipart()
+    //         ->attach("profile", file_get_contents(__DIR__ . '/../../resources/rambutkorea.jpg') , "profile.jpg")
+    //         ->post("https://enkizmujozksi.x.pipedream.net", [
+    //             "username" => "admin",
+    //             "password" => "admin"
+    //         ]);
+    //     self::assertTrue($response->ok());
+    // }
+
+    public function testJSON()
+    {
+        $response = Http::asJson()
+            ->post("https://enkizmujozksi.x.pipedream.net", [
+                "username" => "admin",
+                "password" => "admin"
+            ]);
+        self::assertTrue($response->ok());
+        $response->throw();
+    }
+
+    public function testTimeout()
+    {
+        $response = Http::timeout(1)->asJson()
+            ->post("https://enkizmujozksi.x.pipedream.net", [
+                "username" => "admin",
+                "password" => "admin"
+            ]);
+        self::assertTrue($response->ok());
+    }
+
+    public function testRetry()
+    {
+        $response = Http::timeout(1)->retry(5, 1000)->asJson()
+            ->post("https://enkizmujozksi.x.pipedream.net", [
+                "username" => "admin",
+                "password" => "admin"
+            ]);
+        self::assertTrue($response->ok());
+    }
+
+    public function testThrowError()
+    {
+        $this->assertThrows(function (){
+            $response = Http::get("https://baturhotspring.com/not-found");
+            self::assertEquals(404, $response->status());
+            $response->throw();
+        }, RequestException::class);
+    }
+}
